9f89b99817b7dea182534b6b2c833112a3df179f
tongsq19 <tongsq14@mails.tsinghua.edu.cn>
2019-03-11 15:33:41 +0800

    Modify Test Case in Scenario2 and Scenario4

--- src/main/java/com/tw/StudentGrade.java
+++ src/main/java/com/tw/StudentGrade.java
@@ -16,9 +16,9 @@ public class StudentGrade {
                 answer = console.ask("请输入学生信息（格式：姓名, 学号, 学科: 成绩, ...），按回车提交：\n");
             } else if(answer.equals("2")) {
                 answer = console.ask("请输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：\n");
-            } else {
-                answer = console.ask("1. 添加学生\n2. 生成成绩单\n3. 退出请输入你的选择（1～3）：\n");
+                continue;
             }
+            answer = console.ask("1. 添加学生\n2. 生成成绩单\n3. 退出请输入你的选择（1～3）：\n");
         }
 
         System.out.println("Bye!");
--- src/test/java/com/tw/ApplicationTest.java
+++ src/test/java/com/tw/ApplicationTest.java
@@ -15,24 +15,22 @@ public class ApplicationTest {
         verify(mock).ask("1. 添加学生\n2. 生成成绩单\n3. 退出请输入你的选择（1～3）：\n");
     }
 
-//    @Test
-//    public void Scenario2() throws Exception {
-//        InputHandle mock = mock(InputHandle.class);
-//        when(mock.ask("1. 添加学生\n2. 生成成绩单\n3. 退出请输入你的选择（1～3）：\n")).thenReturn("1");
-//        when(mock.ask("请输入学生信息（格式：姓名, 学号, 学科: 成绩, ...），按回车提交：\n")).thenReturn("张三, ");
-//        when(mock.ask(anyString())).thenReturn("3");
-//
-//        (new StudentGrade()).shell(mock);
-//
-//        verify(mock).ask("1. 添加学生\n2. 生成成绩单\n3. 退出请输入你的选择（1～3）：\n");
-//        verify(mock).ask("请输入学生信息（格式：姓名, 学号, 学科: 成绩, ...），按回车提交：\n");
-//        verify(mock).ask(anyString());
-//    }
+    @Test
+    public void Scenario2() throws Exception {
+        InputHandle mock = mock(InputHandle.class);
+        when(mock.ask("1. 添加学生\n2. 生成成绩单\n3. 退出请输入你的选择（1～3）：\n")).thenReturn("1").thenReturn("3");
+        when(mock.ask("请输入学生信息（格式：姓名, 学号, 学科: 成绩, ...），按回车提交：\n")).thenReturn("张三, 1, 数学: 75, 语文: 95, 英语: 80, 编程: 80");
+
+        (new StudentGrade()).shell(mock);
+
+        verify(mock,times(2)).ask("1. 添加学生\n2. 生成成绩单\n3. 退出请输入你的选择（1～3）：\n");
+        verify(mock).ask("请输入学生信息（格式：姓名, 学号, 学科: 成绩, ...），按回车提交：\n");
+    }
 
     @Test
     public void Scenario3() throws Exception {
         InputHandle mock = mock(InputHandle.class);
-        when(mock.ask("1. 添加学生\n2. 生成成绩单\n3. 退出请输入你的选择（1～3）：\n")).thenReturn("2");
+        when(mock.ask("1. 添加学生\n2. 生成成绩单\n3. 退出请输入你的选择（1～3）：\n")).thenReturn("2").thenReturn("3");
         when(mock.ask("请输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：\n")).thenReturn("3");
 
         (new StudentGrade()).shell(mock);
@@ -44,9 +42,9 @@ public class ApplicationTest {
     @Test
     public void Scenario4() throws Exception {
         InputHandle mock = mock(InputHandle.class);
-        when(mock.ask("1. 添加学生\n2. 生成成绩单\n3. 退出请输入你的选择（1～3）：\n")).thenReturn("1").thenReturn("3");;
+        when(mock.ask("1. 添加学生\n2. 生成成绩单\n3. 退出请输入你的选择（1～3）：\n")).thenReturn("1").thenReturn("3");
         when(mock.ask("请输入学生信息（格式：姓名, 学号, 学科: 成绩, ...），按回车提交：\n")).thenReturn("张三");
-        when(mock.ask("请按正确的格式输入（格式：姓名, 学号, 学科: 成绩, ...）：\n")).thenReturn("3");
+        when(mock.ask("请按正确的格式输入（格式：姓名, 学号, 学科: 成绩, ...）：\n")).thenReturn("数学: 75, 语文: 95, 英语: 80, 编程: 80");
 
         (new StudentGrade()).shell(mock);
 
