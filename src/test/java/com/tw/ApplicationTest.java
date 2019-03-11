adfe09601976cbf38ef1c2569c8fdf3cb63fb56d
tongsq19 <tongsq14@mails.tsinghua.edu.cn>
2019-03-11 18:17:13 +0800

    Add Test and Pass Test for Scenario9

--- src/test/java/com/tw/ApplicationTest.java
+++ src/test/java/com/tw/ApplicationTest.java
@@ -106,4 +106,18 @@ public class ApplicationTest {
         verify(mock).println("成绩单\n姓名|数学|语文|英语|编程|平均分|总分\n========================\n张三|75|95|80|80|82.5|330\n========================\n全班总分平均数：330\n全班总分中位数：330\n");
     }
 
+    @Test
+    public void Scenario9() throws Exception {
+        //生成单个学生成绩，格式正确，学号查询不到
+        InputHandle mock = mock(InputHandle.class);
+        when(mock.ask("1. 添加学生\n2. 生成成绩单\n3. 退出请输入你的选择（1～3）：\n")).thenReturn("1").thenReturn("2").thenReturn("3");
+        when(mock.ask("请输入学生信息（格式：姓名, 学号, 学科: 成绩, ...），按回车提交：\n")).thenReturn("张三, 1, 数学: 75, 语文: 95, 英语: 80, 编程: 80");
+        when(mock.ask("请输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：\n")).thenReturn("2");
+
+        (new StudentGrade()).shell(mock);
+        verify(mock).println("学生张三的成绩被添加");
+        verify(mock).println("成绩单\n姓名|数学|语文|英语|编程|平均分|总分\n========================\n========================\n全班总分平均数：330\n全班总分中位数：330\n");
+    }
+
+
 }
